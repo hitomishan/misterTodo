a7e9cd85f4d099f7c813f2e4f4ee01d096b8bb97
hitomishan <hitomitsuchiya@HitominoMacBook-Pro.local>
2020-03-14 20:13:21 +0900

    プロジェクト削除編集機能作成

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,15 +14,26 @@ class ProjectsController < ApplicationController
     def create
       @project = Project.new(project_params)
       if @project.save
-        # 保存完了したらの処理
-        render '/projects/new'
+        render :new
       else
-        render '/projects/new'
       end
     end
 
+    def edit  
+      @project = Project.find(params[:id])
+    end
+
     def update
-      
+      @project = Project.find(params[:id])
+      if @project.update_attributes(project_params)
+        render :show
+      else
+      end
+    end
+
+    def destroy
+      Project.find(params[:id]).destroy
+        redirect_to projects_url
     end
 
     def project_params
